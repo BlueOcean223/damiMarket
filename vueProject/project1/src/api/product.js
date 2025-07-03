b532b4a6bc0190e560aa9d52aefca1421ac9ac69
SETSUNA <138669985+zhangergou159@users.noreply.github.com>
2025-07-03 23:08:38 +0800

    Delete vueProject/project1 directory

--- vueProject/project1/src/api/product.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export function getProducts(product_id) {
-    return axios.get("http://localhost:8080/api/product",{
-        params:{product_id},
-    });
-}
--- vueProject/project1/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    port: 7070,//端口号
-    proxy: "http://localhost:8080",//代理
-  },
-  
-})
